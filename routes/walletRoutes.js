cd3ff2d9163a3d3bd3e7037579c5f289ee35f05a
Pethum Shyam <139078385+Pethumshyam6611@users.noreply.github.com>
2026-05-02 23:39:15 +0530

    fixed issues

--- routes/walletRoutes.js
+++ routes/walletRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { getProfile, transfer } = require('../controllers/walletController');
+const { getProfile, transfer, getTransactions } = require('../controllers/walletController');
 const protect = require('../middleware/authMiddleware');
 
 const router = express.Router();
